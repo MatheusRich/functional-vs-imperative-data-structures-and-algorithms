3240207fd8080f567760ad20143d8a3a19bfa999
Matheus Richard <matheusrichardt@gmail.com>
2020-11-19 17:19:33 -0300

    Rename files

--- Elm/src/cli.js
+++ Elm/src/cli.js
@@ -1,5 +1,6 @@
-var Elm = require('./main').Elm;
-var main = Elm.Main.init();
+const filename = process.argv.slice(2, 3);
+var Elm = require(`./${filename}`).Elm;
+var main = Elm[filename].init();
 
-process.argv.slice(2).forEach(n => main.ports.get.send(parseInt(n)));
+process.argv.slice(3).forEach(n => main.ports.get.send(parseInt(n)));
 main.ports.put.subscribe(data => console.log(data));
