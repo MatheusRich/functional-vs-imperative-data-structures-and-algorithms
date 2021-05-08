17a6d3b99c0adb118740279fb77d931bc4afaea5
Matheus Richard <matheusrichardt@gmail.com>
2021-05-08 18:54:47 -0300

    Padronize output

--- lexer.rb
+++ lexer.rb
@@ -8,31 +8,38 @@ gemfile do
   gem 'rouge'
 end
 
-ts_source = File.read('./TypeScript/LinkedList.ts')
-hs_source = File.read('./Haskell/LinkedList.hs')
-cpp_source = File.read('./Cpp/LinkedList.hpp')
-elm_source = File.read('./Elm/src/LinkedList.elm')
-ts_count = Rouge::Lexers::Typescript.new.lex(ts_source).filter {!(/\s/ === _1[1])}.count
-hs_count = Rouge::Lexers::Haskell.new.lex(hs_source).filter {!(/\s/ === _1[1])}.count
-cpp_count = Rouge::Lexers::Cpp.new.lex(cpp_source).filter {!(/\s/ === _1[1])}.count
-elm_count = Rouge::Lexers::Elm.new.lex(elm_source).filter {!(/\s/ === _1[1])}.count
+TYPESCRIPT_LEXER = Rouge::Lexers::Typescript.new
+HASKELL_LEXER = Rouge::Lexers::Haskell.new
+CPP_LEXER = Rouge::Lexers::Cpp.new
+ELM_LEXER = Rouge::Lexers::Elm.new
 
-puts "LinkedList.cpp: #{cpp_count} tokens"
-puts "LinkedList.elm: #{elm_count} tokens"
-puts "LinkedList.hs: #{hs_count} tokens"
-puts "LinkedList.ts: #{ts_count} tokens"
+linked_list_ts = File.read('./TypeScript/LinkedList.ts')
+linked_list_hs = File.read('./Haskell/LinkedList.hs')
+linked_list_cpp = File.read('./Cpp/LinkedList.hpp')
+linked_list_elm = File.read('./Elm/src/LinkedList.elm')
+bst_ts = File.read('./TypeScript/BinarySearchTree.ts')
+bst_hs = File.read('./Haskell/BinarySearchTree.hs')
+bst_cpp = File.read('./Cpp/BinarySearchTree.hpp')
+bst_elm = File.read('./Elm/src/BinarySearchTree.elm')
 
-ts_source = File.read('./TypeScript/BinarySearchTree.ts')
-hs_source = File.read('./Haskell/BinarySearchTree.hs')
-cpp_source = File.read('./Cpp/BinarySearchTree.hpp')
-elm_source = File.read('./Elm/src/BinarySearchTree.elm')
-ts_count = Rouge::Lexers::Typescript.new.lex(ts_source).filter {!(/\s/ === _1[1])}.count
-hs_count = Rouge::Lexers::Haskell.new.lex(hs_source).filter {!(/\s/ === _1[1])}.count
-cpp_count = Rouge::Lexers::Cpp.new.lex(cpp_source).filter {!(/\s/ === _1[1])}.count
-elm_count = Rouge::Lexers::Elm.new.lex(elm_source).filter {!(/\s/ === _1[1])}.count
+ll_ts_count = TYPESCRIPT_LEXER.lex(linked_list_ts).filter {!(/\s/ === _1[1])}.count
+ll_hs_count = HASKELL_LEXER.lex(linked_list_hs).filter {!(/\s/ === _1[1])}.count
+ll_cpp_count = CPP_LEXER.lex(linked_list_cpp).filter {!(/\s/ === _1[1])}.count
+ll_elm_count = ELM_LEXER.lex(linked_list_elm).filter {!(/\s/ === _1[1])}.count
 
+bst_ts_count = TYPESCRIPT_LEXER.lex(bst_ts).filter {!(/\s/ === _1[1])}.count
+bst_hs_count = HASKELL_LEXER.lex(bst_hs).filter {!(/\s/ === _1[1])}.count
+bst_cpp_count = CPP_LEXER.lex(bst_cpp).filter {!(/\s/ === _1[1])}.count
+bst_elm_count = ELM_LEXER.lex(bst_elm).filter {!(/\s/ === _1[1])}.count
+
+puts "Linked List"
+puts "  - Cpp: #{ll_cpp_count} tokens"
+puts "  - Elm: #{ll_elm_count} tokens"
+puts "  - Haskell: #{ll_hs_count} tokens"
+puts "  - Typescript: #{ll_ts_count} tokens"
 puts
-puts "BST.cpp: #{cpp_count} tokens"
-puts "BST.elm: #{elm_count} tokens"
-puts "BST.hs: #{hs_count} tokens"
-puts "BST.ts: #{ts_count} tokens"
+puts "Binary Search Tree"
+puts "  - Cpp: #{bst_cpp_count} tokens"
+puts "  - Elm: #{bst_elm_count} tokens"
+puts "  - Haskell: #{bst_hs_count} tokens"
+puts "  - Typescript: #{bst_ts_count} tokens"
