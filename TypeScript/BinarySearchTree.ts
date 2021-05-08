1f37374ca4afe6697a36b9d6845d420035f2930e
Matheus Richard <matheusrichardt@gmail.com>
2021-05-08 15:31:40 -0300

    Remove semicolons

--- TypeScript/BinarySearchTree.ts
+++ TypeScript/BinarySearchTree.ts
@@ -1,109 +1,109 @@
 class TreeNode<T> {
-  value: T;
-  left: TreeNode<T> | null;
-  right: TreeNode<T> | null;
+  value: T
+  left: TreeNode<T> | null
+  right: TreeNode<T> | null
 
   constructor(value: T) {
-    this.value = value;
-    this.left = null;
-    this.right = null;
+    this.value = value
+    this.left = null
+    this.right = null
   }
 
   depth(): number {
-    const leftDepth = this.left?.depth() || 0;
-    const rightDepth = this.right?.depth() || 0;
+    const leftDepth = this.left?.depth() || 0
+    const rightDepth = this.right?.depth() || 0
 
-    return 1 + Math.max(leftDepth, rightDepth);
+    return 1 + Math.max(leftDepth, rightDepth)
   }
 
   push(value: T): TreeNode<T> {
     if (value < this.value) {
-      if (this.left === null) this.left = new TreeNode(value);
-      else this.left.push(value);
+      if (this.left === null) this.left = new TreeNode(value)
+      else this.left.push(value)
     } else if (value > this.value) {
-      if (this.right === null) this.right = new TreeNode(value);
-      else this.right.push(value);
+      if (this.right === null) this.right = new TreeNode(value)
+      else this.right.push(value)
     }
 
-    return this;
+    return this
   }
 
   find(fn: (value: T) => boolean): T | null {
-    if (fn(this.value)) return this.value;
+    if (fn(this.value)) return this.value
 
-    let leftFind = this.left ? this.left.find(fn) : null;
-    if (leftFind) return leftFind;
+    let leftFind = this.left ? this.left.find(fn) : null
+    if (leftFind) return leftFind
 
-    let rightFind = this.right ? this.right.find(fn) : null;
-    if (rightFind) return rightFind;
+    let rightFind = this.right ? this.right.find(fn) : null
+    if (rightFind) return rightFind
 
-    return null;
+    return null
   }
 
   contains(value: T): boolean {
     if (value < this.value) {
-      if (!this.left) return false;
-      return this.left.contains(value);
+      if (!this.left) return false
+      return this.left.contains(value)
     } else if (value > this.value) {
-      if (!this.right) return false;
-      return this.right.contains(value);
+      if (!this.right) return false
+      return this.right.contains(value)
     }
 
-    return true;
+    return true
   }
 
   map(fn: (value: T) => any): TreeNode<any> {
-    let newNode = new TreeNode(fn(this.value));
+    let newNode = new TreeNode(fn(this.value))
 
-    if (this.left) newNode.left = this.left.map(fn);
-    if (this.right) newNode.right = this.right.map(fn);
+    if (this.left) newNode.left = this.left.map(fn)
+    if (this.right) newNode.right = this.right.map(fn)
 
-    return newNode;
+    return newNode
   }
 }
 
 export class BinarySearchTree<T> {
-  root: TreeNode<T> | null;
+  root: TreeNode<T> | null
 
   constructor() {
-    this.root = null;
+    this.root = null
   }
 
   isEmpty(): boolean {
-    return this.root === null;
+    return this.root === null
   }
 
   depth(): number {
-    if (!this.root) return 0;
+    if (!this.root) return 0
 
-    return this.root.depth();
+    return this.root.depth()
   }
 
   push(value: T): BinarySearchTree<T> {
-    if (!this.root) this.root = new TreeNode(value);
-    else this.root.push(value);
+    if (!this.root) this.root = new TreeNode(value)
+    else this.root.push(value)
 
-    return this;
+    return this
   }
 
   find(fn: (value: T) => boolean): T | null {
-    if (!this.root) return null;
+    if (!this.root) return null
 
-    return this.root.find(fn);
+    return this.root.find(fn)
   }
 
   contains(value: T): boolean {
-    if (!this.root) return false;
+    if (!this.root) return false
 
-    return this.root.contains(value);
+    return this.root.contains(value)
   }
 
   map(fn: (value: T) => any): BinarySearchTree<any> {
-    if (!this.root) return new BinarySearchTree();
+    if (!this.root) return new BinarySearchTree()
 
-    let newTree = new BinarySearchTree();
-    newTree.root = this.root.map(fn);
+    let newTree = new BinarySearchTree()
+    newTree.root = this.root.map(fn)
 
-    return newTree;
+    return newTree
   }
 }
--- TypeScript/LinkedList.ts
+++ TypeScript/LinkedList.ts
@@ -1,203 +1,203 @@
 class ListNode<T> {
-  value: T;
-  next: ListNode<T> | null;
+  value: T
+  next: ListNode<T> | null
 
   constructor(value: T, next: ListNode<T> | null = null) {
-    this.value = value;
-    this.next = next;
+    this.value = value
+    this.next = next
   }
 }
 
 export class LinkedList<T> {
-  _head: ListNode<T> | null;
+  _head: ListNode<T> | null
 
   constructor(head: ListNode<T> | null = null) {
-    this._head = head;
+    this._head = head
   }
 
   push(value: T): LinkedList<T> {
-    let newNode = new ListNode(value, this._head);
-    this._head = newNode;
+    let newNode = new ListNode(value, this._head)
+    this._head = newNode
 
-    return this;
+    return this
   }
 
   map(fn: (value: T) => any): LinkedList<any> {
-    if (!this.head()) return new LinkedList<any>();
+    if (!this.head()) return new LinkedList<any>()
 
-    let newList = new LinkedList<any>();
+    let newList = new LinkedList<any>()
 
-    let currentNode = this._head;
+    let currentNode = this._head
     while (currentNode) {
-      newList.push(fn(currentNode.value));
-      currentNode = currentNode.next;
+      newList.push(fn(currentNode.value))
+      currentNode = currentNode.next
     }
 
-    return newList.reverse();
+    return newList.reverse()
   }
 
   filter(fn: (a: T) => boolean): LinkedList<T> {
-    if (!this.head()) return new LinkedList<T>();
+    if (!this.head()) return new LinkedList<T>()
 
-    let newList = new LinkedList<T>();
-    let currentNode = this._head;
+    let newList = new LinkedList<T>()
+    let currentNode = this._head
     while (currentNode) {
-      if (fn(currentNode.value)) newList.push(currentNode.value);
-      currentNode = currentNode.next;
+      if (fn(currentNode.value)) newList.push(currentNode.value)
+      currentNode = currentNode.next
     }
 
-    return newList.reverse();
+    return newList.reverse()
   }
 
   reduce(fn: (a: T, b: T) => T, acc: T): T {
-    if (!this.head()) return acc;
+    if (!this.head()) return acc
 
-    let currentNode = this._head;
+    let currentNode = this._head
     while (currentNode) {
-      acc = fn(currentNode.value, acc);
-      currentNode = currentNode.next;
+      acc = fn(currentNode.value, acc)
+      currentNode = currentNode.next
     }
 
-    return acc;
+    return acc
   }
 
   reverse(): LinkedList<T> {
-    let reversed = new LinkedList<T>();
-    let currentNode = this._head;
+    let reversed = new LinkedList<T>()
+    let currentNode = this._head
 
     while (currentNode) {
-      reversed.push(currentNode.value);
-      currentNode = currentNode.next;
+      reversed.push(currentNode.value)
+      currentNode = currentNode.next
     }
 
-    return reversed;
+    return reversed
   }
 
   head(): T | null {
-    return this._head?.value || null;
+    return this._head?.value || null
   }
 
   tail(): LinkedList<T> {
-    if (!this._head) return new LinkedList<T>();
+    if (!this._head) return new LinkedList<T>()
 
-    return new LinkedList<T>(this._head.next);
+    return new LinkedList<T>(this._head.next)
   }
 
   index(index: number): T | null {
-    if (index < 0 || index >= this.length()) return null;
+    if (index < 0 || index >= this.length()) return null
 
-    let currentIndex: number = 0;
-    let currentNode = this._head;
+    let currentIndex: number = 0
+    let currentNode = this._head
 
     while (currentIndex < index && currentNode && currentNode.next) {
-      currentNode = currentNode.next;
-      currentIndex++;
+      currentNode = currentNode.next
+      currentIndex++
     }
 
-    return currentNode?.value || null;
+    return currentNode?.value || null
   }
 
   last(): T | null {
-    let currentNode = this._head;
+    let currentNode = this._head
     while (currentNode && currentNode.next) {
-      currentNode = currentNode.next;
+      currentNode = currentNode.next
     }
 
-    return currentNode?.value || null;
+    return currentNode?.value || null
   }
 
   isEmpty(): boolean {
-    return this.length() === 0;
+    return this.length() === 0
   }
 
   length(): number {
-    if (!this.head()) return 0;
+    if (!this.head()) return 0
 
-    let length = 0;
-    let currentNode = this._head;
+    let length = 0
+    let currentNode = this._head
     while (currentNode) {
-      length++;
-      currentNode = currentNode.next;
+      length++
+      currentNode = currentNode.next
     }
 
-    return length;
+    return length
   }
 
   mergeSort(): LinkedList<T> {
-    if (this.length() === 1) return this;
+    if (this.length() === 1) return this
 
-    const half = Math.floor(this.length() / 2);
-    const left = this.take(half);
-    const right = this.drop(half);
+    const half = Math.floor(this.length() / 2)
+    const left = this.take(half)
+    const right = this.drop(half)
 
-    return this.merge(left.mergeSort(), right.mergeSort());
+    return this.merge(left.mergeSort(), right.mergeSort())
   }
 
   merge(left: LinkedList<T>, right: LinkedList<T>): LinkedList<T> {
-    let leftNode = left._head;
-    let rightNode = right._head;
-    let newList = new LinkedList<T>();
+    let leftNode = left._head
+    let rightNode = right._head
+    let newList = new LinkedList<T>()
 
     while (leftNode && rightNode) {
       if (leftNode.value < rightNode.value) {
-        newList.push(leftNode.value);
-        leftNode = leftNode.next;
+        newList.push(leftNode.value)
+        leftNode = leftNode.next
       } else {
-        newList.push(rightNode.value);
-        rightNode = rightNode.next;
+        newList.push(rightNode.value)
+        rightNode = rightNode.next
       }
     }
 
     while (leftNode) {
-      newList.push(leftNode.value);
+      newList.push(leftNode.value)
 
-      leftNode = leftNode.next;
+      leftNode = leftNode.next
     }
 
     while (rightNode) {
-      newList.push(rightNode.value);
+      newList.push(rightNode.value)
 
-      rightNode = rightNode.next;
+      rightNode = rightNode.next
     }
 
-    return newList.reverse();
+    return newList.reverse()
   }
 
   take(n: number): LinkedList<T> {
-    let newList = new LinkedList<T>();
-    if (n === 0) return newList;
+    let newList = new LinkedList<T>()
+    if (n === 0) return newList
 
-    let node = this._head;
-    if (!node) return newList;
+    let node = this._head
+    if (!node) return newList
 
     for (let i = 0; i < n; i++) {
-      newList.push(node.value);
-      node = node.next;
+      newList.push(node.value)
+      node = node.next
 
-      if (!node) break;
+      if (!node) break
     }
 
-    return newList.reverse();
+    return newList.reverse()
   }
 
   drop(n: number): LinkedList<T> {
-    let newList = new LinkedList<T>();
-    if (n === this.length()) return newList;
+    let newList = new LinkedList<T>()
+    if (n === this.length()) return newList
 
-    let node = this._head;
-    if (!node) return newList;
+    let node = this._head
+    if (!node) return newList
 
     for (let i = 0; i < n; i++) {
-      node = node.next;
+      node = node.next
 
-      if (!node) break;
+      if (!node) break
     }
 
     while (node) {
-      newList.push(node.value);
-      node = node.next;
+      newList.push(node.value)
+      node = node.next
     }
 
-    return newList.reverse();
+    return newList.reverse()
   }
 }
