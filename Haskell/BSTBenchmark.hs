0240741b651fc128aa9087117be9f60fa4b18604
Matheus Richard <matheusrichardt@gmail.com>
2021-05-09 00:57:58 -0300

    Improve benchmark

--- Haskell/BSTBenchmark.hs
+++ Haskell/BSTBenchmark.hs
@@ -13,10 +13,12 @@ doubleN x = x * 2
 benchmark tree1 tree2 tree3 tree4 tree5 tree6 = defaultMain
   [ bgroup
     "BST contains"
-    [ bench "1 000 elements" $ whnf (BST.contains 501) tree1
-    , bench "10 000 elements" $ whnf (BST.contains 5001) tree2
-    , bench "100 000 elements" $ whnf (BST.contains 50001) tree3
-    , bench "1 000 000 elements" $ whnf (BST.contains 500001) tree4
+    [ bench "10 elements" $ whnf (BST.contains 6) tree1
+    , bench "100 elements" $ whnf (BST.contains 51) tree2
+    , bench "1 000 elements" $ whnf (BST.contains 501) tree3
+    , bench "10 000 elements" $ whnf (BST.contains 5001) tree4
+    , bench "100 000 elements" $ whnf (BST.contains 50001) tree5
+    , bench "1 000 000 elements" $ whnf (BST.contains 500001) tree6
     , bench "10 000 000 elements" $ whnf (BST.contains 5000001) tree5
     , bench "100 000 000 elements" $ whnf (BST.contains 50000001) tree6
     ]
@@ -32,12 +34,11 @@ benchmark tree1 tree2 tree3 tree4 tree5 tree6 = defaultMain
   ]
 
 main = do
-  let tree1 = bstFromList [1 .. 1000]
-  let tree2 = bstFromList [1 .. 10000]
-  let tree3 = bstFromList [1 .. 100000]
-  let tree4 = bstFromList [1 .. 1000000]
-  let tree5 = bstFromList [1 .. 10000000]
-  let tree6 = bstFromList [1 .. 100000000]
+  let tree1 = bstFromList [1 .. 10]
+  let tree2 = bstFromList [1 .. 100]
+  let tree3 = bstFromList [1 .. 1000]
+  let tree4 = bstFromList [1 .. 10000]
+  let tree5 = bstFromList [1 .. 100000]
+  let tree6 = bstFromList [1 .. 1000000]
 
-  benchmark tree1 tree2 tree3 tree4 tree5 tree6
   benchmark tree1 tree2 tree3 tree4 tree5 tree6
