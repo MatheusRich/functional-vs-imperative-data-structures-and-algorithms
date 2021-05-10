6893968976df054502573895b2ff1f8d0bd35905
Matheus Richard <matheusrichardt@gmail.com>
2021-05-10 16:33:33 -0300

    Improve benchmarks

--- Elm/src/LinkedListBenchmark.elm
+++ Elm/src/LinkedListBenchmark.elm
@@ -15,30 +15,34 @@ createList begin end list =
             createList (begin + 1) (end - 1) (LL.push begin list)
 
 
-sortSuite : LL.LinkedList Int -> LL.LinkedList Int -> LL.LinkedList Int -> Benchmark
-sortSuite list1 list2 list3 =
+isOdd x =
+    remainderBy 2 x == 1
+
+
+sortSuite list1 list2 list3 list4 =
     Benchmark.describe "Sort"
-        [ Benchmark.benchmark "100 elements" (\_ -> LL.mergeSort list1)
-        , Benchmark.benchmark "1000 elements" (\_ -> LL.mergeSort list2)
-        , Benchmark.benchmark "10 000 elements" (\_ -> LL.mergeSort list3)
+        [ Benchmark.benchmark "10 elements" (\_ -> LL.mergeSort list1)
+        , Benchmark.benchmark "100 elements" (\_ -> LL.mergeSort list2)
+        , Benchmark.benchmark "1 000 elements" (\_ -> LL.mergeSort list3)
+        , Benchmark.benchmark "10 000 elements" (\_ -> LL.mergeSort list4)
         ]
 
 
-filterSuite : LL.LinkedList Int -> LL.LinkedList Int -> LL.LinkedList Int -> Benchmark
-filterSuite list1 list2 list3 =
+filterSuite list1 list2 list3 list4 =
     Benchmark.describe "Filter"
-        [ Benchmark.benchmark "100 elements" (\_ -> LL.filter (\x -> remainderBy 2 x == 1) list1)
-        , Benchmark.benchmark "1000 elements" (\_ -> LL.filter (\x -> remainderBy 2 x == 1) list2)
-        , Benchmark.benchmark "10 000 elements" (\_ -> LL.filter (\x -> remainderBy 2 x == 1) list3)
+        [ Benchmark.benchmark "10 elements" (\_ -> LL.filter isOdd list1)
+        , Benchmark.benchmark "100 elements" (\_ -> LL.filter isOdd list2)
+        , Benchmark.benchmark "1 000 elements" (\_ -> LL.filter isOdd list3)
+        , Benchmark.benchmark "10 000 elements" (\_ -> LL.filter isOdd list4)
         ]
 
 
-reduceSuite : LL.LinkedList Int -> LL.LinkedList Int -> LL.LinkedList Int -> Benchmark
-reduceSuite list1 list2 list3 =
+reduceSuite list1 list2 list3 list4 =
     Benchmark.describe "Reduce"
-        [ Benchmark.benchmark "100 elements" (\_ -> LL.reduce (+) 0 list1)
-        , Benchmark.benchmark "1000 elements" (\_ -> LL.reduce (+) 0 list2)
-        , Benchmark.benchmark "10 000 elements" (\_ -> LL.reduce (+) 0 list3)
+        [ Benchmark.benchmark "10 elements" (\_ -> LL.reduce (+) 0 list1)
+        , Benchmark.benchmark "100 elements" (\_ -> LL.reduce (+) 0 list2)
+        , Benchmark.benchmark "1 000 elements" (\_ -> LL.reduce (+) 0 list3)
+        , Benchmark.benchmark "10 000 elements" (\_ -> LL.reduce (+) 0 list4)
         ]
 
 
@@ -46,17 +50,20 @@ main : BenchmarkProgram
 main =
     let
         list1 =
-            createList 0 100 LL.Empty
+            createList 0 10 LL.Empty
 
         list2 =
-            createList 0 1000 LL.Empty
+            createList 0 100 LL.Empty
 
         list3 =
+            createList 0 1000 LL.Empty
+
+        list4 =
             createList 0 10000 LL.Empty
     in
     program <|
-        Benchmark.describe "Linked List +"
-            [ sortSuite list1 list2 list3
-            , filterSuite list1 list2 list3
-            , reduceSuite list1 list2 list3
+        Benchmark.describe "Linked List"
+            [ sortSuite list1 list2 list3 list4
+            , filterSuite list1 list2 list3 list4
+            , reduceSuite list1 list2 list3 list4
             ]
--- TypeScript/LinkedListBenchmark.ts
+++ TypeScript/LinkedListBenchmark.ts
@@ -11,7 +11,16 @@ function createList(size: number): LinkedList<number> {
   return list;
 }
 
-async function sortSuite(l1: LinkedList<number>, l2: LinkedList<number>, l3: LinkedList<number>) {
+function isOdd(n: number): boolean {
+  return n / 2 == 1;
+}
+
+async function sortSuite(
+  l1: LinkedList<number>,
+  l2: LinkedList<number>,
+  l3: LinkedList<number>,
+  l4: LinkedList<number>
+) {
   let measurement = await measure(() => l1.mergeSort());
   console.log(`L1 sort Mean: ${measurement.mean} ms`);
 
@@ -19,21 +28,37 @@ async function sortSuite(l1: LinkedList<number>, l2: LinkedList<number>, l3: Lin
   console.log(`L2 sort Mean: ${measurement.mean} ms`);
 
   measurement = await measure(() => l3.mergeSort());
-  console.log(`L3 sort Mean: ${measurement.mean} ms\n`);
+  console.log(`L3 sort Mean: ${measurement.mean} ms`);
+
+  measurement = await measure(() => l4.mergeSort());
+  console.log(`L4 sort Mean: ${measurement.mean} ms`);
 }
 
-async function filterSuite(l1: LinkedList<number>, l2: LinkedList<number>, l3: LinkedList<number>) {
-  let measurement = await measure(() => l1.filter(n => n / 2 == 1));
+async function filterSuite(
+  l1: LinkedList<number>,
+  l2: LinkedList<number>,
+  l3: LinkedList<number>,
+  l4: LinkedList<number>
+) {
+  let measurement = await measure(() => l1.filter(isOdd));
   console.log(`L1 filter Mean: ${measurement.mean} ms`);
 
-  measurement = await measure(() => l2.filter(n => n / 2 == 1));
+  measurement = await measure(() => l2.filter(isOdd));
   console.log(`L2 filter Mean: ${measurement.mean} ms`);
 
-  measurement = await measure(() => l3.filter(n => n / 2 == 1));
-  console.log(`L3 filter Mean: ${measurement.mean} ms\n`);
+  measurement = await measure(() => l3.filter(isOdd));
+  console.log(`L3 filter Mean: ${measurement.mean} ms`);
+
+  measurement = await measure(() => l4.filter(isOdd));
+  console.log(`L4 filter Mean: ${measurement.mean} ms`);
 }
 
-async function reduceSuite(l1: LinkedList<number>, l2: LinkedList<number>, l3: LinkedList<number>) {
+async function reduceSuite(
+  l1: LinkedList<number>,
+  l2: LinkedList<number>,
+  l3: LinkedList<number>,
+  l4: LinkedList<number>
+) {
   let measurement = await measure(() => l1.reduce((a, b) => a + b, 0));
   console.log(`L1 reduce Mean: ${measurement.mean} ms`);
 
@@ -42,15 +67,20 @@ async function reduceSuite(l1: LinkedList<number>, l2: LinkedList<number>, l3: L
 
   measurement = await measure(() => l3.reduce((a, b) => a + b, 0));
   console.log(`L3 reduce Mean: ${measurement.mean} ms`);
+
+  measurement = await measure(() => l4.reduce((a, b) => a + b, 0));
+  console.log(`L4 reduce Mean: ${measurement.mean} ms`);
 }
 
-let list1 = createList(1000);
-let list2 = createList(10000);
-let list3 = createList(100000);
+let list1 = createList(10);
+let list2 = createList(100);
+let list3 = createList(1000);
+let list4 = createList(10000);
 
 const bench = async () => {
-  await sortSuite(list1, list2, list3);
-  await filterSuite(list1, list2, list3);
-  await reduceSuite(list1, list2, list3);
+  await sortSuite(list1, list2, list3, list4);
+  await filterSuite(list1, list2, list3, list4);
+  await reduceSuite(list1, list2, list3, list4);
 };
-bench()
+
+bench();
