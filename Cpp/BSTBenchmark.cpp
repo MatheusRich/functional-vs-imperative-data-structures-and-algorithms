35ee0ad77e17a6a760102c5054b4d919d965697b
Matheus Richard <matheusrichardt@gmail.com>
2021-05-19 16:34:33 -0300

    Update benchmarks

--- Cpp/BSTBenchmark.cpp
+++ Cpp/BSTBenchmark.cpp
@@ -5,123 +5,72 @@
 
 using namespace std;
 
-int double_n(int n) { return n * 2; }
+long long double_n(long long n) { return n * 2; }
 
-vector<int>* listFromRange(int begin, int end) {
-  auto l = new vector<int>();
+vector<long long>* listFromRange(long long begin, long long end) {
+  auto l = new vector<long long>();
 
-  for (int i = begin - 1; i < end; i++) l->push_back(i + 1);
+  for (long long i = begin - 1; i < end; i++) l->push_back(i + 1);
 
   return l;
 }
 
-void treeFromList(vector<int> list, BinarySearchTree<int>* tree) {
+void treeFromList(vector<long long> list, BinarySearchTree<long long>* tree) {
   if (list.empty()) return;
   if (list.size() == 1) {
     tree->push(list.front());
     return;
   };
 
-  int middleIdx = floor(list.size() / 2.0);
+  long long middleIdx = floor(list.size() / 2.0);
   tree->push(list[middleIdx]);
 
-  treeFromList(vector<int>(list.begin(), list.begin() + middleIdx), tree);
-  treeFromList(vector<int>(list.begin() + middleIdx + 1, list.end()), tree);
+  treeFromList(vector<long long>(list.begin(), list.begin() + middleIdx), tree);
+  treeFromList(vector<long long>(list.begin() + middleIdx + 1, list.end()),
+               tree);
 }
 
-BinarySearchTree<int>* treeFromRange(int from, int to) {
-  auto tree = new BinarySearchTree<int>();
+BinarySearchTree<long long>* treeFromRange(long long from, long long to) {
+  auto tree = new BinarySearchTree<long long>();
   treeFromList(*listFromRange(from, to), tree);
 
   return tree;
 }
 
-auto tree1 = treeFromRange(1, 10);
-auto tree2 = treeFromRange(1, 100);
-auto tree3 = treeFromRange(1, 1000);
-auto tree4 = treeFromRange(1, 10000);
-auto tree5 = treeFromRange(1, 100000);
-auto tree6 = treeFromRange(1, 1000000);
+static void BM_Contains(benchmark::State& state) {
+  auto size = state.range(0);
+  auto tree = treeFromRange(1, size);
 
-static void BM_ContainsT1(benchmark::State& state) {
-  for (auto _ : state) tree1->contains(10);
+  for (auto _ : state) tree->contains(size);
 }
 
-static void BM_ContainsT2(benchmark::State& state) {
-  for (auto _ : state) tree2->contains(100);
+static void BM_Map(benchmark::State& state) {
+  auto size = state.range(0);
+  auto tree = treeFromRange(1, size);
+
+  for (auto _ : state) tree->map(double_n);
 }
 
-static void BM_ContainsT3(benchmark::State& state) {
-  for (auto _ : state) tree3->contains(1000);
-}
+BENCHMARK(BM_Contains)
+    ->Arg(10)
+    ->Arg(100)
+    ->Arg(1000)
+    ->Arg(10000)
+    ->Arg(100000)
+    ->Arg(250000)
+    ->Arg(500000)
+    ->Arg(750000)
+    ->Arg(1000000);
 
-static void BM_ContainsT4(benchmark::State& state) {
-  for (auto _ : state) tree4->contains(10000);
-}
-
-static void BM_ContainsT5(benchmark::State& state) {
-  for (auto _ : state) tree5->contains(100000);
-}
-
-static void BM_ContainsT6(benchmark::State& state) {
-  for (auto _ : state) tree6->contains(1000000);
-}
-
-static void BM_MapT1(benchmark::State& state) {
-  for (auto _ : state) tree1->map(double_n);
-}
-
-static void BM_MapT2(benchmark::State& state) {
-  for (auto _ : state) tree2->map(double_n);
-}
-
-static void BM_MapT3(benchmark::State& state) {
-  for (auto _ : state) tree3->map(double_n);
-}
-
-static void BM_MapT4(benchmark::State& state) {
-  for (auto _ : state) tree4->map(double_n);
-}
-
-static void BM_MapT5(benchmark::State& state) {
-  for (auto _ : state) tree5->map(double_n);
-}
-
-static void BM_MapT6(benchmark::State& state) {
-  for (auto _ : state) tree6->map(double_n);
-}
-
-// int main(int argc, char const* argv[]) {
-  // auto list = listFromRange(1, 10);
-  // int middleIdx = floor(list->size() / 2);
-  // auto v = vector<int>(list->begin(), list->begin() + middleIdx);
-
-  // for (auto i : v) {
-  //   cout << i << " ";
-  // }
-  // cout << endl;
-
-  // auto v2 = vector<int>(list->begin() + middleIdx, list->end());
-
-  // for (auto i : v2) {
-  //   cout << i << " ";
-  // }
-  // cout << endl;
-//   cout << endl << tree1->debug() << endl;
-//   return 0;
-// }
-
-BENCHMARK(BM_ContainsT1);
-BENCHMARK(BM_ContainsT2);
-BENCHMARK(BM_ContainsT3);
-BENCHMARK(BM_ContainsT4);
-BENCHMARK(BM_ContainsT5);
-BENCHMARK(BM_ContainsT6);
-BENCHMARK(BM_MapT1);
-BENCHMARK(BM_MapT2);
-BENCHMARK(BM_MapT3);
-BENCHMARK(BM_MapT4);
-BENCHMARK(BM_MapT5);
-BENCHMARK(BM_MapT6);
+BENCHMARK(BM_Map)
+    ->Arg(10)
+    ->Arg(100)
+    ->Arg(1000)
+    ->Arg(10000)
+    ->Arg(100000)
+    ->Arg(250000)
+    ->Arg(500000)
+    ->Arg(750000)
+    ->Arg(1000000);
 
 BENCHMARK_MAIN();
--- Elm/src/LinkedListBenchmark.elm
+++ Elm/src/LinkedListBenchmark.elm
@@ -19,30 +19,42 @@ isOdd x =
     remainderBy 2 x == 1
 
 
-sortSuite list1 list2 list3 list4 =
+sortSuite list1 list2 list3 list4 list5 list6 list7 list8 =
     Benchmark.describe "Sort"
         [ Benchmark.benchmark "10 elements" (\_ -> LL.mergeSort list1)
         , Benchmark.benchmark "100 elements" (\_ -> LL.mergeSort list2)
-        , Benchmark.benchmark "1 000 elements" (\_ -> LL.mergeSort list3)
-        , Benchmark.benchmark "10 000 elements" (\_ -> LL.mergeSort list4)
+        , Benchmark.benchmark "500 elements" (\_ -> LL.mergeSort list3)
+        , Benchmark.benchmark "1000 elements" (\_ -> LL.mergeSort list4)
+        , Benchmark.benchmark "2500 elements" (\_ -> LL.mergeSort list5)
+        , Benchmark.benchmark "5000 elements" (\_ -> LL.mergeSort list6)
+        , Benchmark.benchmark "7500 elements" (\_ -> LL.mergeSort list7)
+        , Benchmark.benchmark "10000 elements" (\_ -> LL.mergeSort list8)
         ]
 
 
-filterSuite list1 list2 list3 list4 =
+filterSuite list1 list2 list3 list4 list5 list6 list7 list8 =
     Benchmark.describe "Filter"
         [ Benchmark.benchmark "10 elements" (\_ -> LL.filter isOdd list1)
         , Benchmark.benchmark "100 elements" (\_ -> LL.filter isOdd list2)
-        , Benchmark.benchmark "1 000 elements" (\_ -> LL.filter isOdd list3)
-        , Benchmark.benchmark "10 000 elements" (\_ -> LL.filter isOdd list4)
+        , Benchmark.benchmark "500 elements" (\_ -> LL.filter isOdd list3)
+        , Benchmark.benchmark "1000 elements" (\_ -> LL.filter isOdd list4)
+        , Benchmark.benchmark "2500 elements" (\_ -> LL.filter isOdd list5)
+        , Benchmark.benchmark "5000 elements" (\_ -> LL.filter isOdd list6)
+        , Benchmark.benchmark "7500 elements" (\_ -> LL.filter isOdd list7)
+        , Benchmark.benchmark "10000 elements" (\_ -> LL.filter isOdd list8)
         ]
 
 
-reduceSuite list1 list2 list3 list4 =
+reduceSuite list1 list2 list3 list4 list5 list6 list7 list8 =
     Benchmark.describe "Reduce"
         [ Benchmark.benchmark "10 elements" (\_ -> LL.reduce (+) 0 list1)
         , Benchmark.benchmark "100 elements" (\_ -> LL.reduce (+) 0 list2)
-        , Benchmark.benchmark "1 000 elements" (\_ -> LL.reduce (+) 0 list3)
-        , Benchmark.benchmark "10 000 elements" (\_ -> LL.reduce (+) 0 list4)
+        , Benchmark.benchmark "500 elements" (\_ -> LL.reduce (+) 0 list3)
+        , Benchmark.benchmark "1000 elements" (\_ -> LL.reduce (+) 0 list4)
+        , Benchmark.benchmark "2500 elements" (\_ -> LL.reduce (+) 0 list5)
+        , Benchmark.benchmark "5000 elements" (\_ -> LL.reduce (+) 0 list6)
+        , Benchmark.benchmark "7500 elements" (\_ -> LL.reduce (+) 0 list7)
+        , Benchmark.benchmark "10000 elements" (\_ -> LL.reduce (+) 0 list8)
         ]
 
 
@@ -56,14 +68,23 @@ main =
             createList 0 100 LL.Empty
 
         list3 =
-            createList 0 1000 LL.Empty
+            createList 0 500 LL.Empty
 
         list4 =
+            createList 0 1000 LL.Empty
+
+        list5 =
+            createList 0 2500 LL.Empty
+        list6 =
+            createList 0 5000 LL.Empty
+        list7 =
+            createList 0 7500 LL.Empty
+        list8 =
             createList 0 10000 LL.Empty
     in
     program <|
         Benchmark.describe "Linked List"
-            [ sortSuite list1 list2 list3 list4
-            , filterSuite list1 list2 list3 list4
-            , reduceSuite list1 list2 list3 list4
+            [ sortSuite list1 list2 list3 list4 list5 list6 list7 list8
+            , filterSuite list1 list2 list3 list4 list5 list6 list7 list8
+            , reduceSuite list1 list2 list3 list4 list5 list6 list7 list8
             ]
